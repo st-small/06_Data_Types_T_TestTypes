600d8366a4dd517375e8506c50fdd5bb9ff9ecab
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-01-22 09:03:25 +0200

    Initial Commit

---
+++ TestTypes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTypes
+//
+//  Created by Stas on 22.01.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
